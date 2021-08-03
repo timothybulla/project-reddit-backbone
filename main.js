2fa3195561e3532d7c45c5341e60245cb1a576f8
timothybulla <83347577+timothybulla@users.noreply.github.com>
2021-08-02 20:05:02 -0400

    backbone model and view

--- main.js
+++ main.js
@@ -1,1 +1,40 @@
+var PostModel = Backbone.Model.extend({
+  defaults: function() {
+  return {
+    user: '',
+    text: ''
+   }
+ }
+});
 
+var PostView = Backbone.View.extend({
+
+  template: Handlebars.compile($('#post-template').html()),
+  className: 'post',
+
+  render: function () {
+    this.$el.html(this.template(this.model.attributes));
+
+    return this;
+  }
+});
+
+$('.add-post').on('click', function () {
+  var text = $('#post-name').val();
+  var user = $('#post-user').val();
+
+  var postModel = new PostModel({ text: text, user: user });
+  var postView = new PostView({ model: postModel });
+
+  $('.posts').append(postView.render().el);
+});
+/*
+var postModel = new PostModel({ user: 'Aaron', text: 'Hey man!' });
+
+var postView = new PostView({ model: postModel });
+
+var newPostHTML = postView.render().el;
+
+$('.posts').append(newPostHTML);
+
+*/
